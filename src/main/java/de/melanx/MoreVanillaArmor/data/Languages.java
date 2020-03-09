b7abc83918739253487144bac2b045c852e56635
MelanX <meissi97@me.com>
2020-03-09 16:05:49 +0100

    fixed lowercase first letter in en_us

--- src/main/java/de/melanx/MoreVanillaArmor/data/Languages.java
+++ src/main/java/de/melanx/MoreVanillaArmor/data/Languages.java
@@ -112,6 +112,6 @@ public class Languages {
     }
 
     private static String capitalize(String string) {
-        return string.substring(0, 1).toLowerCase() + string.substring(1);
+        return string.substring(0, 1).toUpperCase() + string.substring(1);
     }
 }
